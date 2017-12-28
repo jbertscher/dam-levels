2c9e71aa9fdf3d7e43a07fc8fe0e39d592dd58b1
Jonathan <jbertscher@gmail.com>
2017-12-28 10:30:39 +0200

    Cleaned up location dictionary

--- download-weather-data.py
+++ download-weather-data.py
@@ -20,12 +20,13 @@ def download_historic_weather_json(api_key, date, geo_location, file_location):
 if __name__ == "__main__":
     print('Starting @ {0}'.format(str(datetime.datetime.now())))
 
-    location = {'Theewaterskloof': '-34.078056,19.289167'}
-    location2 = {'hottentots_holland': '-34.1005,18.9645',
-                 'two_rivers_farm': '-33.873,19.032',
-                 'airport': '-33.965,18.593'}
+    location = {'Theewaterskloof': '-34.078056,19.289167',
+        'hottentots_holland': '-34.1005,18.9645',
+        'two_rivers_farm': '-33.873,19.032',
+        'airport': '-33.965,18.593'}
 
-    date = datetime.date(year = 2015, month = 5, day = 24)
+    date = datetime.date(year = 2012, month = 1, day = 1)
+    
     day_counter = 0
     min_counter = 0
     for dam, loc in location.items():
