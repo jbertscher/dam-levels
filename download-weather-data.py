d9c30142550c463fb51c73639650fde64cc95750
= <=>
2018-01-06 18:13:38 +0200

    Worked on predictions. Cleaned up structure.

--- download-weather-data.py
+++ download-weather-data.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     # location = {'Theewaterskloof': '-34.078056,19.289167'}
     location = {'Matroosfontein': 'Matroosfontein'}
 
-    date = datetime.date(year = 2012, month = 3, day = 21)
+    date = datetime.date(year = 2012, month = 12, day = 24)
     
     day_counter = 0
     min_counter = 0
